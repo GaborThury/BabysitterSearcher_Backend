eadd53bd6d3a3ea93699ec6d2bce5aa62eab37f3
Gabor Thury <Gabor_Thury@epam.com>
2019-06-11 15:34:27 +0200

    event controller and service

--- src/main/java/com/homeproject/babysitting/app/babysitting/app/controller/EventController.java
+++ src/main/java/com/homeproject/babysitting/app/babysitting/app/controller/EventController.java
@@ -1,4 +1,74 @@
 package com.homeproject.babysitting.app.babysitting.app.controller;
 
+import com.homeproject.babysitting.app.babysitting.app.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+@RestController
+@RequestMapping("/events")
 public class EventController {
+
+    @Autowired
+    private EventService eventService;
+
+    @GetMapping("/{name}")
+    public ResponseEntity findEvent(@PathVariable(value = "name") String name) {
+        return ResponseEntity.ok(eventService.findEventByName(name));
+    }
+
+    @GetMapping("/")
+    public ResponseEntity findAllEvents() {
+        try {
+            return ResponseEntity.ok(eventService.findAll());
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/eventnames")
+    public ResponseEntity findAllEventNames() {
+        try {
+            return ResponseEntity.ok(eventService.findAllEventNames());
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error while getting eventnames data");
+        }
+    }
+
+    @PostMapping("/")
+    public ResponseEntity createEvent(@RequestBody Map<String, Object> map) {
+        if (eventNameExistsAndNotEmpty(map)) {
+            String name = map.get("name").toString();
+            return ResponseEntity.ok(eventService.create(name, map));
+        } else {
+            return ResponseEntity.badRequest().body("Attribute 'name' cannot be empty or null!");
+        }
+    }
+
+    @PutMapping("/")
+    public ResponseEntity updateEvent(@RequestBody Map<String, Object> map) {
+        if (eventNameExistsAndNotEmpty(map)) {
+            String name = map.get("name").toString();
+            return ResponseEntity.ok(eventService.update(name, map));
+        } else {
+            return ResponseEntity.badRequest().body("Attribute 'name' cannot be empty or null!");
+        }
+    }
+
+    private boolean eventNameExistsAndNotEmpty(Map<String, Object> map) {
+        try {
+            String name = map.get("name").toString();
+            if (name.isEmpty()) throw new IllegalArgumentException();
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
 }
--- src/main/java/com/homeproject/babysitting/app/babysitting/app/service/EventService.java
+++ src/main/java/com/homeproject/babysitting/app/babysitting/app/service/EventService.java
@@ -1,4 +1,38 @@
 package com.homeproject.babysitting.app.babysitting.app.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+@Service
 public class EventService {
+
+    @Autowired
+    private FirestoreService firestoreService;
+
+    private static final String EVENT_COLLECTION = "events";
+
+
+    public List<Map<String, Object>> findAll() throws ExecutionException, InterruptedException {
+        return firestoreService.getAllDocumentsFromCollection(EVENT_COLLECTION);
+    }
+
+    public List<String> findAllEventNames() throws ExecutionException, InterruptedException {
+        return firestoreService.getAllDocumentNamesFromCollection(EVENT_COLLECTION);
+    }
+
+    public Map<String, Object> findEventByName(String eventName) {
+        return firestoreService.getDocumentFields(EVENT_COLLECTION, eventName);
+    }
+
+    public Map<String, Object> create(String eventName, Map<String, Object> values) {
+        return firestoreService.createFireStoreDocument(EVENT_COLLECTION, eventName, values);
+    }
+
+    public Map<String, Object> update(String eventName, Map<String, Object> values) {
+        return firestoreService.updateFireStoreDocument(EVENT_COLLECTION, eventName, values);
+    }
 }
