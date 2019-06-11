2fa9938c54b7361c3c317aa5657fdcd24b0097ba
Gabor Thury <Gabor_Thury@epam.com>
2019-06-11 17:21:45 +0200

    advertisement controller and service added

--- src/main/java/com/homeproject/babysitting/app/babysitting/app/controller/AdvertisementController.java
+++ src/main/java/com/homeproject/babysitting/app/babysitting/app/controller/AdvertisementController.java
@@ -1,4 +1,31 @@
 package com.homeproject.babysitting.app.babysitting.app.controller;
 
+import com.homeproject.babysitting.app.babysitting.app.service.AdvertisementService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.ExecutionException;
+
+@RestController
+@RequestMapping
+@CrossOrigin(origins = "http://localhost:3000")
 public class AdvertisementController {
+
+    @Autowired
+    private AdvertisementService advertisementService;
+
+    @GetMapping
+    public ResponseEntity findAll() {
+        try {
+            return ResponseEntity.ok(advertisementService.findAll());
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 }
--- src/main/java/com/homeproject/babysitting/app/babysitting/app/service/AdvertisementService.java
+++ src/main/java/com/homeproject/babysitting/app/babysitting/app/service/AdvertisementService.java
@@ -1,4 +1,39 @@
 package com.homeproject.babysitting.app.babysitting.app.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+@Service
 public class AdvertisementService {
+
+    @Autowired
+    private FirestoreService firestoreService;
+
+    private static final String ADVERTISEMENT_COLLECTION = "advertisements";
+
+
+    public List<Map<String, Object>> findAll() throws ExecutionException, InterruptedException {
+        return firestoreService.getAllDocumentsFromCollection(ADVERTISEMENT_COLLECTION);
+    }
+
+    public List<String> findAllAdvertisementIdS() throws ExecutionException, InterruptedException {
+        return firestoreService.getAllDocumentNamesFromCollection(ADVERTISEMENT_COLLECTION);
+    }
+
+    public Map<String, Object> findAdvertisementById(String name) {
+        return firestoreService.getDocumentFields(ADVERTISEMENT_COLLECTION, name);
+    }
+
+    public Map<String, Object> create(String name, Map<String, Object> values) {
+        return firestoreService.createFireStoreDocument(ADVERTISEMENT_COLLECTION, name, values);
+    }
+
+    public Map<String, Object> update(String name, Map<String, Object> values) {
+        return firestoreService.updateFireStoreDocument(ADVERTISEMENT_COLLECTION, name, values);
+    }
+
 }
